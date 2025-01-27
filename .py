3cdbb78ec0a65ea1d186f1cf3328aa201f15ff8a
nuta222 <147779063+nuta222@users.noreply.github.com>
2025-01-27 10:14:07 +0000

    Add files via upload

---
+++ .py
@@ -0,0 +1,24 @@
+# Main program
+If __name__ == “__main__”:
+    Account = BankAccount()
+
+    While True:
+        Print(“\n1. Deposit”)
+        Print(“2. Withdraw”)
+        Print(“3. Check Balance”)
+        Print(“4. Exit”)
+
+        Choice = input(“Enter choice: “)
+
+        If choice == ‘1’:
+            Amount = float(input(“Enter deposit amount: “))
+            Account.deposit(amount)
+        Elif choice == ‘2’:
+            Amount = float(input(“Enter withdrawal amount: “))
+            Account.withdraw(amount)
+        Elif choice == ‘3’:
+            Account.check_balance()
+        Elif choice == ‘4’:
+            Break
+        Else:
+            Print(“Invalid choice. Please try again.”)
